74fef1c4002c6ad4264f67389d59424d94d64e90
cnubodicerla <cnubodicherla999@gmail.com>
2024-10-08 16:25:34 +0000

    web2

--- src/com/srk/dao/web.java
+++ src/com/srk/dao/web.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/devops")
-public class DevOpsServlet extends HttpServlet {
+public class web extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
